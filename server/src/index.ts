ac01efb1b29f4b627b96ce3594cbf35a2b0df378
babu rao <baburao@kali.kali>
2025-09-04 20:28:37 +0530

    added server client mangement

---
+++ server/src/index.ts
@@ -0,0 +1,13 @@
+import type { Request, Response } from 'express';
+import express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, TypeScript + Node.js Server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
